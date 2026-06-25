96cbd217e4a829c5beb0416d3deb2990a4b5d5d3
Ganesh Verma <bibekverma21@gmail.com>
2026-06-25 08:20:36 +0545

    complitely implemented watchlist component

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Tooltip, Grow } from '@mui/material';
 
-import {KeyboardArrowDown, KeyboardArrowUp} from "@mui/icons-material"
+import { BarChartOutlined, KeyboardArrowDown, KeyboardArrowUp, MoreHoriz } from "@mui/icons-material"
 
 import { watchlist } from '../data/data';
 
@@ -52,10 +52,59 @@ const WatchListItem = ({ stock }) => {
             <KeyboardArrowDown className="down" />
           ) : (
             <KeyboardArrowUp className="up" />
-      )}
-      <span className="price">{stock.price}</span>
+          )}
+          <span className="price">{stock.price}</span>
         </div>
       </div>
+      {showWatchlistActions && <WatchListActions uid={stock.name} />}
     </li>
+  );
+};
+
+const WatchListActions = ({ uid }) => {
+  return (
+    <span className="actions">
+      <span>
+        <Tooltip
+          title="Buy (B)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="buy">Buy</button>
+        </Tooltip>
+
+        <Tooltip
+          title="Sell (S)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="sell">Sell</button>
+        </Tooltip>
+
+        <Tooltip
+          title="Analytics (A)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="action">
+            <BarChartOutlined className="icon" />
+          </button>
+        </Tooltip>
+
+        <Tooltip
+          title="More"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+           <button className="action">
+            <MoreHoriz className="icon" />
+          </button>
+        </Tooltip>
+      </span>
+    </span>
   )
 }
